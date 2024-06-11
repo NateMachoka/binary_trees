fde8275f5a637c446c2473fd9d9302c3b172e224
Nathan Machoka <natemachoka@gmail.com>
2024-06-11 00:39:58 -0700

    fixed 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,11 +8,14 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height = 0;
+	size_t right_height = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	if (left_height > right_height)
 		return (left_height + 1);
@@ -28,11 +31,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-
 	int left_height = 0;
 	int right_height = 0;
+
+	if (tree == NULL)
+		return (0);
 
 	if (tree->left != NULL)
 		left_height = binary_tree_height(tree->left);
